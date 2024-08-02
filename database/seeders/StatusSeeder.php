d589c6827125d81d69aef79b8bd9e6f65efdc81c
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-08-01 23:01:43 -0500

    NTS

--- database/seeders/StatusSeeder.php
+++ database/seeders/StatusSeeder.php
@@ -2,16 +2,35 @@
 
 namespace Database\Seeders;
 
+use App\Models\Status;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Facades\Excel;
 
 class StatusSeeder extends Seeder
 {
+
+    use Importable;
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        Excel::import(new class implements ToModel
+        {
+            public function model(array $row)
+            {
+                if (!is_numeric($row[0])) return null;
+
+                Status::updateOrCreate([
+                    'id' => $row[0]
+                ], [
+                    'name' => $row[1],
+                    'description' => $row[2]
+                ]);
+            }
+        }, 'storage/app/utils/Statuses.xlsx');
     }
 }
